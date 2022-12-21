402001a2305a1944bb5d6a0fd15a605ec2dab12d
atakanakyildiz <atakanmembership@gmail.com>
2022-12-21 11:48:18 +0100

    This code about tranform mRNA to amino acid names

--- 2.2 genes.py
+++ 2.2 genes.py
@@ -1,12 +1,13 @@
 
 FILENAME = "genetic_code.csv"
+
+
 def readfile(filename):
     try:
         with open(filename) as my_file:
             my_return = []
             file = my_file.readlines()
             for lines in file:
-                lines = lines.replace(" ", '')
                 lines = lines.replace('"', '')
                 lines = lines.replace(" ", '')
                 lines = lines.replace("\n", "")
@@ -30,7 +31,39 @@ def genetic_code_dict(filename):
     return genetic_code
 
 def main():
-    genetic_code_dict(FILENAME)
+    given_codons = "GUAUGCACCUCGUCGUGACUUUCCUCAUGAGCUGAUGAAGCU"
+    #   GUAUGCACGUGACUUUCCUCAUGAGCUGAU
+    genetic_codes = genetic_code_dict(FILENAME)
+
+    try:
+        if len(given_codons.split("GUG", 1)[1]) > len(given_codons.split("AUG", 1)[1]):
+            our_codons = "GUG"+given_codons.split("GUG", 1)[1]
+        else:
+            our_codons = "AUG"+given_codons.split("AUG", 1)[1]
+        print(our_codons)
+
+        #   Secondly, we need to divide them to three and make a list:
+        codons_list = [(our_codons[i:i + 3]) for i in range(0, len(our_codons), 3)]
+        for element in codons_list:
+            if len(element) != 3:
+                codons_list.remove(element)
+        print(codons_list)
+
+        #   Thirdly, we need to tranform our mRNA to acid names:
+        result_str = ''
+        for codons in codons_list:
+            for acid_names, codes in genetic_codes.items():
+                for cod in codes:
+                    if codons == cod:
+                        result_str = result_str + acid_names
+
+        result_str = "".join(result_str.split("start"))
+        print(result_str)
+
+    except IndexError:
+        print("In mRNA there is no start codon")
+
+
 
 
 if __name__ == '__main__':
